e30a26d5664431a7acf205330128065d72d6e676
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2017-02-27 12:00:11 +0900

    indices2num

--- storages/log_store.py
+++ storages/log_store.py
@@ -3,93 +3,90 @@ import numpy as np
 import itertools
 import operator
 
+value_labels = [('P1', 'FIT101'),
+('P1', 'LIT101'),
+('P2', 'AIT201'),
+('P2', 'AIT202'),
+('P2', 'AIT203'),
+('P2', 'FIT201'),
+('P3', 'DPIT301'),
+('P3', 'FIT301'),
+('P3', 'LIT301'),
+('P4', 'AIT401'),
+('P4', 'AIT402'),
+('P4', 'FIT401'),
+('P4', 'LIT401'),
+('P5', 'AIT501'),
+('P5', 'AIT502'),
+('P5', 'AIT503'),
+('P5', 'AIT504'),
+('P5', 'FIT501'),
+('P5', 'FIT502'),
+('P5', 'FIT503'),
+('P5', 'FIT504'),
+('P5', 'PIT501'),
+('P5', 'PIT502'),
+('P5', 'PIT503'),
+('P6', 'FIT601') ]
+
+discrete_labels = [
+('P1', 'MV101'),
+('P1', 'P101'),
+('P1', 'P102'),
+('P2', 'MV201'),
+('P2', 'P201'),
+('P2', 'P202'),
+('P2', 'P203'),
+('P2', 'P204'),
+('P2', 'P205'),
+('P2', 'P206'),
+('P3', 'MV301'),
+('P3', 'MV302'),
+('P3', 'MV303'),
+('P3', 'MV304'),
+('P3', 'P301'),
+('P3', 'P302'),
+('P4', 'P401'),
+('P4', 'P402'),
+('P4', 'P403'),
+('P4', 'P404'),
+('P4', 'UV401'),
+('P5', 'P501'),
+('P5', 'P502'),
+('P6', 'P601'),
+('P6', 'P602'),
+('P6', 'P603'),]
+
+
 class LogStore:
     def __init__(self, filename):
-        log = pd.read_excel(filename, header=[0, 1])
-        log.index = log.index.to_datetime()
+        self.log = pd.read_excel(filename, header=[0, 1])
+        log.index = self.log.index.to_datetime()
         self.last_day = log.tail(1).index[0].date()
 
-        value_labels = [('P1', 'FIT101'),
-        ('P1', 'LIT101'),
-        ('P2', 'AIT201'),
-        ('P2', 'AIT202'),
-        ('P2', 'AIT203'),
-        ('P2', 'FIT201'),
-        ('P3', 'DPIT301'),
-        ('P3', 'FIT301'),
-        ('P3', 'LIT301'),
-        ('P4', 'AIT401'),
-        ('P4', 'AIT402'),
-        ('P4', 'FIT401'),
-        ('P4', 'LIT401'),
-        ('P5', 'AIT501'),
-        ('P5', 'AIT502'),
-        ('P5', 'AIT503'),
-        ('P5', 'AIT504'),
-        ('P5', 'FIT501'),
-        ('P5', 'FIT502'),
-        ('P5', 'FIT503'),
-        ('P5', 'FIT504'),
-        ('P5', 'PIT501'),
-        ('P5', 'PIT502'),
-        ('P5', 'PIT503'),
-        ('P6', 'FIT601') ]
         zscore = lambda x: (x - x.mean()) / x.std()
-        self.values = log[value_labels].apply(zscore)
+        self.values = self.log[value_labels].apply(zscore)
 
-        discrete_labels = [
-        ('P1', 'MV101'),
-        ('P1', 'P101'),
-        ('P1', 'P102'),
-        ('P2', 'MV201'),
-        ('P2', 'P201'),
-        ('P2', 'P202'),
-        ('P2', 'P203'),
-        ('P2', 'P204'),
-        ('P2', 'P205'),
-        ('P2', 'P206'),
-        ('P3', 'MV301'),
-        ('P3', 'MV302'),
-        ('P3', 'MV303'),
-        ('P3', 'MV304'),
-        ('P3', 'P301'),
-        ('P3', 'P302'),
-        ('P4', 'P401'),
-        ('P4', 'P402'),
-        ('P4', 'P403'),
-        ('P4', 'P404'),
-        ('P4', 'UV401'),
-        ('P5', 'P501'),
-        ('P5', 'P502'),
-        ('P6', 'P601'),
-        ('P6', 'P602'),
-        ('P6', 'P603'),]
-        indices = log[discrete_labels]
+        self.indices = self.log[discrete_labels]
 
-        index_set = set(map(tuple, log[discrete_labels].values.tolist()))
-        index_list= sorted(list(index_set))
-        indices2num = dict(zip(index_list, itertools.count(1)))
-        log[('num', '')] = indices.apply(lambda x: indices2num[tuple(x)], axis=1)
-        self.indices = log[discrete_labels + [('num', '')]]
-        self.indices2num = indices2num
-        self.maxnum = max(indices2num.items(), key=operator.itemgetter(1))[1]
+    def eval_seq(self, indices2num):
+        self.log[('num', '')] = indices.apply(lambda x: indices2num[tuple(x)], axis=1)
+        self.indices = self.log[discrete_labels + [('num', '')]]
 
-    def eval_seq(self):
-        values = self.values
-        inices = self.indices
+        value_shape = self.values.values.shape
+        indices_shape = self.indices.values.shape
 
-        grouped_by_day = values.groupby(lambda x: x.date)
-        dates = grouped_by_day.groups.key()
-        values_seq = []
-        indices_seq = []
-        for date in dates:
-            values_by_day = values.loc[pd.Series(grouped_by_day.groups[date])]
-            indices_by_day = indices.loc[pd.Series(grouped_by_day.groups[date])]
-            values_seq.append(values_by_day.values)
-            indices_seq.append(indices_by_day.values)
-        return (nq.array(values_seq).T, nq.array(indices_seq).T)
+        values_seq = self.values.values.reshape(value_shape[0], 1, value_shape[1])
+        indices_seq = self.indices.values.reshape(indices_shape[0], 1, indices_shape[1])
+        return (values_seq, indices_seq)
 
     def training_seq(self):
+        if self.indices2num == None:
+            index_set = set(map(tuple, self.log[discrete_labels].values.tolist()))
+            index_list= sorted(list(index_set))
+            self.indices2num = dict(zip(index_list, itertools.count(1)))
+            self.maxnum = max(indices2num.items(), key=operator.itemgetter(1))[1]
+
         values = self.values[self.values.index.date != self.last_day]
         inices = self.indices[self.values.index.date != self.last_day]
 
@@ -105,16 +102,18 @@ class LogStore:
         return (nq.array(values_seq).T, nq.array(indices_seq).T)
 
     def test_seq(self):
+        if self.indices2num == None:
+            index_set = set(map(tuple, self.log[discrete_labels].values.tolist()))
+            index_list= sorted(list(index_set))
+            self.indices2num = dict(zip(index_list, itertools.count(1)))
+            self.maxnum = max(indices2num.items(), key=operator.itemgetter(1))[1]
+
         values = self.values[self.values.index.date == self.last_day]
         inices = self.indices[self.values.index.date == self.last_day]
 
-        grouped_by_day = values.groupby(lambda x: x.date)
-        dates = grouped_by_day.groups.key()
-        values_seq = []
-        indices_seq = []
-        for date in dates:
-            values_by_day = values.loc[pd.Series(grouped_by_day.groups[date])]
-            indices_by_day = indices.loc[pd.Series(grouped_by_day.groups[date])]
-            values_seq.append(values_by_day.values)
-            indices_seq.append(indices_by_day.values)
-        return (nq.array(values_seq).T, nq.array(indices_seq).T)
+        value_shape = values.values.shape
+        indices_shape = indices.values.shape
+
+        values_seq = values.values.reshape(value_shape[0], 1, value_shape[1])
+        indices_seq = indices.values.reshape(indices_shape[0], 1, indices_shape[1])
+        return (values_seq, indices_seq)
