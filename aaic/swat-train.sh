0e25dfdb679f334cc0a2f6af704cee37bf537d30
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2017-06-27 17:15:49 +0900

    train script for AAIC

---
+++ aaic/swat-train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS --group=g-sarg
+#PBS -q gq
+#PBS -b 1
+#PBS -l cpunum_job=5
+#PBS -l gpunum_job=1
+#PBS -l elapstim_req=72:00:00
+#PBS -l memsz_job=30gb
+#PBS -M yoriyuki.yamagata@aist.go.jp
+#PBS -m b,e
+
+arrGPU=(${CUDA_VISIBLE_DEVICE//,/ })
+GPU=${arrGPU[0]}
+
+python training.py -n 100 -i 100 -s 1 -c true -d true -a sigmoid -g $GPU SWaT_Dataset_Normal_v0 &
+python training.py -n 200 -i 100 -s 1 -c true -d true -a sigmoid -g $GPU SWaT_Dataset_Normal_v0 &
+python training.py -n 300 -i 100 -s 1 -c true -d true -a sigmoid -g $GPU SWaT_Dataset_Normal_v0 &
+python training.py -n 400 -i 100 -s 1 -c true -d true -a sigmoid -g $GPU SWaT_Dataset_Normal_v0 &
+python training.py -n 500 -i 100 -s 1 -c true -d true -a sigmoid -g $GPU SWaT_Dataset_Normal_v0 &
+
+wait
