9e5ef18407d3d86f7c43841c140be6ca57bb135c
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2017-07-05 17:36:20 +0900

    script

--- aaic/swat-train-all.sh
+++ aaic/swat-train-all.sh
@@ -16,13 +16,13 @@ source activate swat-analyzer
 # echo $GPU
 
 cd /home/yoriyuki/Factory/swat-analyzer
-python training.py -n 100 -i 200 -s 1 -c true -d true -a relu -g 0 SWaT_Dataset_Normal_v0 &
-python training.py -n 200 -i 300 -s 1 -c true -d true -a relu -g 1 SWaT_Dataset_Normal_v0 &
-python training.py -n 300 -i 400 -s 1 -c true -d true -a relu -g 2 SWaT_Dataset_Normal_v0 &
-python training.py -n 400 -i 500 -s 1 -c true -d true -a relu -g 3 SWaT_Dataset_Normal_v0 &
-python training.py -n 500 -i 200 -s 1 -c true -d true -a sigmoid -g 4 SWaT_Dataset_Normal_v0 &
-python training.py -n 600 -i 300 -s 1 -c true -d true -a sigmoid -g 5 SWaT_Dataset_Normal_v0 &
-python training.py -n 700 -i 400 -s 1 -c true -d true -a sigmoid -g 6 SWaT_Dataset_Normal_v0 &
-python training.py -n 800 -i 500 -s 1 -c true -d true -a sigmoid -g 7 SWaT_Dataset_Normal_v0 &
+python training.py -n 200 -i 100 -s 1 -c true -d true -a relu -g 0 SWaT_Dataset_Normal_v0 &
+python training.py -n 300 -i 100 -s 1 -c true -d true -a relu -g 1 SWaT_Dataset_Normal_v0 &
+python training.py -n 400 -i 100 -s 1 -c true -d true -a relu -g 2 SWaT_Dataset_Normal_v0 &
+python training.py -n 500 -i 100 -s 1 -c true -d true -a relu -g 3 SWaT_Dataset_Normal_v0 &
+python training.py -n 200 -i 100 -s 1 -c true -d true -a sigmoid -g 4 SWaT_Dataset_Normal_v0 &
+python training.py -n 300 -i 100 -s 1 -c true -d true -a sigmoid -g 5 SWaT_Dataset_Normal_v0 &
+python training.py -n 400 -i 100 -s 1 -c true -d true -a sigmoid -g 6 SWaT_Dataset_Normal_v0 &
+python training.py -n 500 -i 100 -s 1 -c true -d true -a sigmoid -g 7 SWaT_Dataset_Normal_v0 &
 
 wait
