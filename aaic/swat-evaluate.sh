fa20022526c20ab813ed93f8e42efe6619336e36
Yoriyuki Yamagata <yoriyuki.y@gmail.com>
2017-07-13 19:17:21 +0900

    bug fix

--- aaic/swat-evaluate.sh
+++ aaic/swat-evaluate.sh
@@ -7,7 +7,7 @@
 #PBS -M yoriyuki.yamagata@aist.go.jp
 #PBS -m e
 
-#source activate swat-analyzer
+source activate swat-analyzer
 
 cd ${WD}
 python evaluate.py -n ${N_UNITS} -s 1 -a sigmoid -g -1 output/SWaT_Dataset_Normal_v0-model-1-${N_UNITS}-dropout-True-sigmoid-${ITER}-lstms.npzSWaT_Dataset_Normal_v0 SWaT_Dataset_Attack_v0 output/SWaT_Dataset_Attack_v0-scores-1-${N_UNITS}-dropout-True-sigmoid-${ITER}.csv
